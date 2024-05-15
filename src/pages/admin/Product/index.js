9dd799947df6b3b39aaaf1c7433aca6099cea770
phunguyen <phumapuit>
2024-05-15 21:43:00 +0700

    update layout product and user

--- src/pages/admin/Product/index.js
+++ src/pages/admin/Product/index.js
@@ -1,61 +1,92 @@
-import { Row, Col, Card, Radio, Table, Upload, message, Progress, Button, Avatar, Typography } from "antd";
-
-import { Link } from "react-router-dom";
-
-// Images
-import ava1 from "../../../assets/images/logo-shopify.svg";
-import ava2 from "../../../assets/images/logo-atlassian.svg";
-import ava3 from "../../../assets/images/logo-slack.svg";
-import ava5 from "../../../assets/images/logo-jira.svg";
-import ava6 from "../../../assets/images/logo-invision.svg";
-import face from "../../../assets/images/face-1.jpg";
-import face2 from "../../../assets/images/face-2.jpg";
-import face3 from "../../../assets/images/face-3.jpg";
-import face4 from "../../../assets/images/face-4.jpg";
-import face5 from "../../../assets/images/face-5.jpeg";
-import face6 from "../../../assets/images/face-6.jpeg";
-import pencil from "../../../assets/images/pencil.svg";
-import React, {useEffect} from "react";
-import {useDispatch, useSelector} from "react-redux";
-import {getUsersList} from "../../../reducers/actions/user";
-import {getProductList} from "../../../reducers/actions/product";
+import { Card, Radio, Table, Avatar } from "antd";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getProductList } from "../../../reducers/actions/product";
 import avatar from "../../../assets/images/avatarUser.png";
+import Tooltip from "antd/es/tooltip";
 
-const { Title } = Typography;
-
-// table code start
 const columns = [
+    {
+        title: "HỈNH ẢNH",
+        dataIndex: "image",
+        key: "image",
+        width: "10%",
+        fixed: "left",
+    },
     {
         title: "TÊN SẢN PHẨM",
         dataIndex: "nameProduct",
         key: "nameProduct",
         width: "30%",
+        ellipsis: {
+            showTitle: false,
+        },
+        render: (nameProduct) => (
+            <Tooltip placement="topLeft" title={nameProduct}>
+                {nameProduct}
+            </Tooltip>
+        ),
+        sorter: (a, b) => a.nameProduct.localeCompare(b.nameProduct),
+        fixed: "left",
     },
     {
         title: "SỐ LƯỢNG",
         dataIndex: "quantity",
         key: "quantity",
+        width: "10%",
+        sorter: (a, b) => a.quantity - b.quantity,
     },
 
     {
         title: "ĐƠN GIÁ",
         key: "price",
         dataIndex: "price",
+        width: "10%",
+        sorter: (a, b) => a.price - b.price,
     },
     {
         title: "MÔ TẢ",
         key: "description",
         dataIndex: "description",
         width: "30%",
+        ellipsis: {
+            showTitle: false,
+        },
+        render: (description) => (
+            <Tooltip placement="topLeft" title={description}>
+                {description}
+            </Tooltip>
+        ),
     },
     {
         title: "GIẢM GIÁ",
         key: "discount",
         dataIndex: "discount",
+        width: "10%",
+        sorter: (a, b) => a.discount - b.discount,
+    },
+    {
+        title: "THAO TÁC",
+        key: "action",
+        dataIndex: "action",
+        width: "10%",
+        align: "right",
+        fixed: "right",
     },
 ];
 
 export default function Product() {
+    const [selectedRowKeys, setSelectedRowKeys] = useState([]);
+    const onSelectChange = (newSelectedRowKeys) => {
+        console.log("selectedRowKeys changed: ", newSelectedRowKeys);
+        setSelectedRowKeys(newSelectedRowKeys);
+    };
+    const rowSelection = {
+        selectedRowKeys,
+        onChange: onSelectChange,
+    };
+    const hasSelected = selectedRowKeys.length > 0; // DÙNG BIẾN NÀY LẤY DANH SÁCH MÃ ID ĐƯỢC CHECK, ĐỂ XÁC ĐỊNH DISABLE NUT DELETE ĐI HAY KHÔNG
+
     const dispatch = useDispatch();
     useEffect(() => {
         console.log("DISPATCH ACTION GET PRODUCT LIST");
@@ -63,60 +94,37 @@ export default function Product() {
     }, [dispatch]);
 
     const { productList, loading, error } = useSelector((state) => state.products);
-    if (loading){
-        return <h1>TẢI LÂU VCLLLLLLLLLLLLLLLLLL</h1>
+    if (loading) {
+        return <h1>TẢI LÂU VCLLLLLLLLLLLLLLLLLL</h1>;
     }
-    if (error!== null){
-        return <h1>TẮT SERVER CMNRRRRRRRRRRRRRRRRRRR</h1>
+    if (error !== null) {
+        return <h1>TẮT SERVER CMNRRRRRRRRRRRRRRRRRRR</h1>;
     }
+
     let dataProduct = [];
     productList?.data?.map((productItem, index) => {
         return dataProduct.push({
-            key: productItem['productId'],
-            nameProduct: (
+            key: productItem["productId"],
+            image: (
                 <>
                     <Avatar.Group>
-                        <Avatar className="shape-avatar" shape="square" size={40} src={productItem['images'] ? "http://localhost:8080"+productItem['images'] : avatar} />
-                        <div className="avatar-info">
-                            <Title level={5}>{productItem['product_name']}</Title>
-                        </div>
+                        <Avatar className="shape-avatar" shape="square" size={40} src={productItem["images"] ? "http://localhost:8080" + productItem["images"] : avatar} />
                     </Avatar.Group>{" "}
                 </>
             ),
-            quantity: (
+            nameProduct: productItem["product_name"],
+            quantity: productItem["product_quantity"],
+            price: productItem["product_price"],
+            description: productItem["product_description"],
+            discount: productItem["product_discount"] === null ? 0 : productItem["product_discount"],
+            action: (
                 <>
-                    <div className="author-info">
-                        <Title level={5}>{productItem['product_quantity']}</Title>
-                    </div>
-                </>
-            ),
-
-            price: (
-                <>
-                    <Button type="primary" className="tag-primary">
-                        {productItem['product_price']}
-                    </Button>
-                </>
-            ),
-            description: (
-                <>
-                    <div className="author-info">
-                        <Title level={5}>{productItem['product_description']}</Title>
-                    </div>
-                </>
-            ),
-            discount: (
-                <>
-                    <div className="ant-employed">
-                        <span>{productItem['product_discount']===null?"Không":productItem['product_discount']}</span>
-                        <a href="">Edit</a>
-                    </div>
+                    <a href="">Edit</a>
                 </>
             ),
         });
     });
     const onChange = (e) => console.log(`radio checked:${e.target.value}`);
-
     return (
         <>
             <div className="tabled">
@@ -134,7 +142,18 @@ export default function Product() {
                     }
                 >
                     <div className="table-responsive">
-                        <Table columns={columns} dataSource={dataProduct} pagination={true} className="ant-border-space" />
+                        <Table
+                            columns={columns}
+                            dataSource={dataProduct}
+                            pagination={true}
+                            showSorterTooltip={false}
+                            scroll={{
+                                x: 1500,
+                                y: 500,
+                            }}
+                            rowSelection={rowSelection}
+                            className="ant-border-space"
+                        />
                     </div>
                 </Card>
             </div>
--- src/pages/admin/User/index.js
+++ src/pages/admin/User/index.js
@@ -1,35 +1,85 @@
-import { Row, Col, Card, Radio, Table, Upload, message, Button, Avatar, Typography } from "antd";
+import { Card, Radio, Table, Avatar, Typography } from "antd";
 import avatar from "../../../assets/images/avatarUser.png";
-import React, { memo, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getUsersList } from "../../../reducers/actions/user";
-import userReducer from "../../../reducers/UserReducer";
-
-const { Title } = Typography;
-
+import Tooltip from "antd/es/tooltip";
+import moment from "moment";
 // table code start
 const columns = [
+    {
+        title: "HỈNH ẢNH",
+        dataIndex: "image",
+        key: "image",
+        width: "10%",
+        fixed: "left",
+    },
     {
         title: "TÊN NGƯỜI DÙNG",
         dataIndex: "nameUser",
         key: "nameUser",
-        width: "32%",
+        width: "15%",
+        ellipsis: {
+            showTitle: false,
+        },
+        render: (nameProduct) => (
+            <Tooltip placement="topLeft" title={nameProduct}>
+                {nameProduct}
+            </Tooltip>
+        ),
+        sorter: (a, b) => a.nameUser.localeCompare(b.nameUser),
+        fixed: "left",
+    },
+    {
+        title: "EMAIL",
+        dataIndex: "emailUser",
+        key: "emailUser",
+        width: "15%",
+        ellipsis: {
+            showTitle: false,
+        },
+        render: (emailUser) => (
+            <Tooltip placement="topLeft" title={emailUser}>
+                {emailUser}
+            </Tooltip>
+        ),
+        sorter: (a, b) => a.emailUser.localeCompare(b.emailUser),
     },
     {
         title: "ĐỊA CHỈ",
         dataIndex: "addressUser",
         key: "addressUser",
+        width: "15%",
+        ellipsis: {
+            showTitle: false,
+        },
+        render: (emailUser) => (
+            <Tooltip placement="topLeft" title={emailUser}>
+                {emailUser}
+            </Tooltip>
+        ),
+        sorter: (a, b) => a.addressUser.localeCompare(b.addressUser),
     },
 
     {
         title: "SỐ ĐIỆN THOẠI",
         key: "phoneNumberUser",
         dataIndex: "phoneNumberUser",
+        width: "15%",
     },
     {
         title: "NGÀY SINH",
         key: "dateOfBirdUser",
         dataIndex: "dateOfBirdUser",
+        sorter: (a, b) => moment(a.dateOfBirdUser, "DD/MM/YYYY").toDate() - moment(b.dateOfBirdUser, "DD/MM/YYYY").toDate(),
+    },
+    {
+        title: "THAO TÁC",
+        key: "action",
+        dataIndex: "action",
+        width: "10%",
+        align: "right",
+        fixed: "right",
     },
 ];
 
@@ -40,50 +90,44 @@ export default function User() {
         console.log("DISPATCH ACTION GET USER LIST");
         dispatch(getUsersList());
     }, [dispatch]);
+    const [selectedRowKeys, setSelectedRowKeys] = useState([]);
+    const onSelectChange = (newSelectedRowKeys) => {
+        console.log("selectedRowKeys changed: ", newSelectedRowKeys);
+        setSelectedRowKeys(newSelectedRowKeys);
+    };
+    const rowSelection = {
+        selectedRowKeys,
+        onChange: onSelectChange,
+    };
+    const hasSelected = selectedRowKeys.length > 0; // DÙNG BIẾN NÀY LẤY DANH SÁCH MÃ ID ĐƯỢC CHECK, ĐỂ XÁC ĐỊNH DISABLE NUT DELETE ĐI HAY KHÔNG
 
     const { usersList, loading, error } = useSelector((state) => state.users);
-    if (loading){
-        return <h1>TẢI LÂU VCLLLLLLLLLLLLLLLLLL</h1>
+    if (loading) {
+        return <h1>TẢI LÂU VCLLLLLLLLLLLLLLLLLL</h1>;
     }
-    if (error!== null){
-        return <h1>TẮT SERVER CMNRRRRRRRRRRRRRRRRRRR</h1>
+    if (error !== null) {
+        return <h1>TẮT SERVER CMNRRRRRRRRRRRRRRRRRRR</h1>;
     }
     let dataUser = [];
+    let dateFormat = "";
     usersList?.data?.map((userItem, index) => {
         return dataUser.push({
-            key: userItem['userId'],
-            nameUser: (
+            key: userItem["userId"],
+            image: (
                 <>
                     <Avatar.Group>
-                        <Avatar className="shape-avatar" shape="square" size={40} src={userItem.fullName ? "http://localhost:8080"+userItem['avatar'] : avatar} />
-                        <div className="avatar-info">
-                            <Title level={5}>{userItem['fullName']}</Title>
-                            <p>{userItem['email']}</p>
-                        </div>
+                        <Avatar className="shape-avatar" shape="square" size={40} src={userItem["images"] ? "http://localhost:8080" + userItem["images"] : avatar} />
                     </Avatar.Group>{" "}
                 </>
             ),
-            addressUser: (
+            nameUser: userItem["fullName"],
+            emailUser: userItem["email"],
+            addressUser: userItem["address"],
+            phoneNumberUser: userItem["phoneNumber"],
+            dateOfBirdUser: userItem["dob"],
+            action: (
                 <>
-                    <div className="author-info">
-                        <Title level={5}>{userItem['address']}</Title>
-                    </div>
-                </>
-            ),
-
-            phoneNumberUser: (
-                <>
-                    <Button type="primary" className="tag-primary">
-                        {userItem['phoneNumber']}
-                    </Button>
-                </>
-            ),
-            dateOfBirdUser: (
-                <>
-                    <div className="ant-employed">
-                        <span>{userItem['dob']}</span>
-                        <a href="">Edit</a>
-                    </div>
+                    <a href="">Edit</a>
                 </>
             ),
         });
@@ -106,7 +150,18 @@ export default function User() {
                     }
                 >
                     <div className="table-responsive">
-                        <Table columns={columns} dataSource={dataUser} pagination={true} className="ant-border-space" />
+                        <Table
+                            columns={columns}
+                            dataSource={dataUser}
+                            pagination={true}
+                            showSorterTooltip={false}
+                            scroll={{
+                                x: 1500,
+                                y: 500,
+                            }}
+                            rowSelection={rowSelection}
+                            className="ant-border-space"
+                        />
                     </div>
                 </Card>
             </div>
